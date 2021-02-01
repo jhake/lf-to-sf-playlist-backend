ee0e98ed1752c48681e40800a7a40a53ff14923f
Rommel Monsayac <rommelmonsayac@gmail.com>
2021-02-01 13:49:08 +0800

    Fix merging from main

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/auth/spotify/callback", to: "sessions#callback"
   get "/top_tracks", to: "spotify#get_top_tracks"
-<<<<<<< HEAD
-  post "/top_tracks", to: "spotify#get_top_tracks"
-  get "/lf_top_tracks", to: "spotify#get_lf_top_tracks"
-
-  delete "/logout", to: "sessions#logout"
-=======
   get "/search_spotify_tracks", to: "spotify#search_spotify_tracks"
   get "/lf_get_request", to: "lastfm#get_request"
 
   post "/create_playlist", to: "spotify#create_playlist"
->>>>>>> 4ac4ca88850de90d5f551801eca959eb4e9c7133
 end
